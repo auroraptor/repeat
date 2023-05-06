483af1481c794db194d62db39ec8b80657f13433
auroraptor <interestingbutno@yandex.ru>
2023-05-06 22:33:20 +0300

    feat: add play icon to Узнать button

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,19 +5,19 @@ import { Theme, Props } from "../../types";
 type ThemeStyles = {
   [key in Theme]: {
     color: string;
-  }
-}
+  };
+};
 
 export function Dashboard({ theme }: Props) {
   const lead = require("../../images/lead-illustration.png");
 
   // Определение стилей для темы
-  const themeStyles:ThemeStyles = {
+  const themeStyles: ThemeStyles = {
     light: {
-      color: 'rgba(121, 63, 180, 1)',
+      color: "rgba(121, 63, 180, 1)",
     },
     dark: {
-      color: 'rgba(255, 255, 255, 1)',
+      color: "rgba(255, 255, 255, 1)",
     },
   };
 
@@ -33,8 +33,25 @@ export function Dashboard({ theme }: Props) {
           to={"/"}
           className={`${styles.link} ${styles[theme]}`}
           style={{ color: themeStyles[theme].color }}
+          onClick={() => alert('👩🏼‍🏫 📚 👩🏼‍🎓')}
         >
           Узнать
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M28.9669 13.6668L6.38415 2.08949C4.58733 1.17066 2.42297 2.47743 2.42297 4.49885V27.6534C2.42297 29.6748 4.56691 31.002 6.38415 30.0627L28.9669 18.4855C30.9271 17.485 30.9271 14.6673 28.9669 13.6668Z"
+              fill={themeStyles[theme].color}
+              stroke={themeStyles[theme].color}
+              stroke-width="3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </Link>
       </div>
       <img src={lead} alt="Asking question illustration"></img>
