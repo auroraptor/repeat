e69c8a643152ead62abe2afee3ee4b9b90202456
auroraptor <interestingbutno@yandex.ru>
2023-05-09 21:24:36 +0300

    feat: implement CloudsBackground component

---
+++ src/components/CloudsBackground/CloudsBackground.tsx
@@ -0,0 +1,14 @@
+import styles from "./CloudsBackground.module.scss";
+
+export function CloudsBackground() {
+    return (
+      <div className={styles.cloudContainer}>
+        <div className={`${styles.cloudPart} ${styles.part1}`}></div>
+        <div className={`${styles.cloudPart} ${styles.part2}`}></div>
+        <div className={`${styles.cloudPart} ${styles.part3}`}></div>
+        <div className={`${styles.cloudPart} ${styles.part4}`}></div>
+        <div className={`${styles.cloudPart} ${styles.part5}`}></div>
+      </div>
+    );
+  }
+  
